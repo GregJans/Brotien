6b517173147ef1495712e2f956ca9e5c67d892fb
Gregory Jans <gjans0430@floridapoly.edu>
2023-12-06 19:32:01 -0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './constants/colors';
 
 export default function App() {
+  console.log('testing');
+  let x = 1;
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello Bros 2</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.red,
     alignItems: 'center',
     justifyContent: 'center',
   },
